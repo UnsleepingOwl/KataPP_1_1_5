701d60adc8f9c5bba42cc86b41b4cf8fa8a37ec9
UnsleepingOwl <lion2000_07@mail.ru>
2022-11-06 20:01:10 +0300

    .rollback()

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
     private SessionFactory sessionFactory = Util.getSessionFactory();
+
     public UserDaoHibernateImpl() {
 
     }
@@ -16,43 +17,63 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
         try (Session session = sessionFactory.getCurrentSession()) {
-            session.beginTransaction();
-            session.createSQLQuery("CREATE TABLE IF NOT EXISTS User (" +
-                            "id BIGINT NOT NULL AUTO_INCREMENT," +
-                            "name VARCHAR(64)," +
-                            "lastName VARCHAR(64)," +
-                            "age SMALLINT," +
-                            "PRIMARY KEY ( id ) )")
-                    .executeUpdate();
-            session.getTransaction().commit();
+            try {
+                session.beginTransaction();
+                session.createSQLQuery("CREATE TABLE IF NOT EXISTS User (" +
+                                "id BIGINT NOT NULL AUTO_INCREMENT," +
+                                "name VARCHAR(64)," +
+                                "lastName VARCHAR(64)," +
+                                "age SMALLINT," +
+                                "PRIMARY KEY ( id ) )")
+                        .executeUpdate();
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+                session.getTransaction().rollback();
+            }
         }
     }
 
     @Override
     public void dropUsersTable() {
         try (Session session = sessionFactory.getCurrentSession()) {
-            session.beginTransaction();
-            session.createSQLQuery("DROP TABLE IF EXISTS User").executeUpdate();
-            session.getTransaction().commit();
+            try {
+                session.beginTransaction();
+                session.createSQLQuery("DROP TABLE IF EXISTS User").executeUpdate();
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+                session.getTransaction().rollback();
+            }
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
         try (Session session = sessionFactory.getCurrentSession()) {
-            session.beginTransaction();
-            session.save(new User(name, lastName, age));
-            session.getTransaction().commit();
+            try {
+                session.beginTransaction();
+                session.save(new User(name, lastName, age));
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+                session.getTransaction().rollback();
+            }
         }
     }
 
     @Override
     public void removeUserById(long id) {
         try (Session session = sessionFactory.getCurrentSession()) {
-            session.beginTransaction();
-            User user = session.get(User.class, id);
-            session.delete(user);
-            session.getTransaction().commit();
+            try {
+                session.beginTransaction();
+                User user = session.get(User.class, id);
+                session.delete(user);
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+                session.getTransaction().rollback();
+            }
         }
     }
 
@@ -67,9 +88,14 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void cleanUsersTable() {
         try (Session session = sessionFactory.getCurrentSession()) {
-            session.beginTransaction();
-            session.createQuery("DELETE FROM User").executeUpdate();
-            session.getTransaction().commit();
+            try {
+                session.beginTransaction();
+                session.createQuery("DELETE FROM User").executeUpdate();
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+                session.getTransaction().rollback();
+            }
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -17,15 +17,20 @@ public class UserDaoJDBCImpl implements UserDao {
     public void createUsersTable() {
         try (Connection connection = Util.getConnection();
              Statement statement = connection.createStatement()) {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS USERS (" +
-                    "id BIGINT NOT NULL AUTO_INCREMENT," +
-                    "name VARCHAR(64)," +
-                    "lastName VARCHAR(64)," +
-                    "age SMALLINT," +
-                    "PRIMARY KEY ( id ) )"
-            );
+            try {
+                statement.executeUpdate("CREATE TABLE IF NOT EXISTS USERS (" +
+                        "id BIGINT NOT NULL AUTO_INCREMENT," +
+                        "name VARCHAR(64)," +
+                        "lastName VARCHAR(64)," +
+                        "age SMALLINT," +
+                        "PRIMARY KEY ( id ) )"
+                );
+            } catch (SQLException e) {
+                e.printStackTrace();
+                connection.rollback();
+            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
@@ -33,9 +38,14 @@ public class UserDaoJDBCImpl implements UserDao {
     public void dropUsersTable() {
         try (Connection connection = Util.getConnection();
              Statement statement = connection.createStatement()) {
-            statement.executeUpdate("DROP TABLE IF EXISTS users");
+            try {
+                statement.executeUpdate("DROP TABLE IF EXISTS users");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                connection.rollback();
+            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
@@ -43,12 +53,17 @@ public class UserDaoJDBCImpl implements UserDao {
     public void saveUser(String name, String lastName, byte age) {
         try (Connection connection = Util.getConnection();
              PreparedStatement prepStat = connection.prepareStatement("INSERT INTO users (name, lastName, age) VALUES(?, ?, ?)")) {
-            prepStat.setString(1, name);
-            prepStat.setString(2, lastName);
-            prepStat.setByte(3, age);
-            prepStat.executeUpdate();
+            try {
+                prepStat.setString(1, name);
+                prepStat.setString(2, lastName);
+                prepStat.setByte(3, age);
+                prepStat.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                connection.rollback();
+            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
@@ -56,10 +71,15 @@ public class UserDaoJDBCImpl implements UserDao {
     public void removeUserById(long id) {
         try (Connection connection = Util.getConnection();
              PreparedStatement prepStat = connection.prepareStatement("DELETE FROM users where id = ?")) {
-            prepStat.setLong(1, id);
-            prepStat.executeUpdate();
+            try {
+                prepStat.setLong(1, id);
+                prepStat.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                connection.rollback();
+            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
@@ -69,17 +89,17 @@ public class UserDaoJDBCImpl implements UserDao {
         try (Connection connection = Util.getConnection();
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery("SELECT * FROM users")) {
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastName"));
-                user.setAge(resultSet.getByte("age"));
-                userList.add(user);
+                while (resultSet.next()) {
+                    User user = new User();
+                    user.setId(resultSet.getLong("id"));
+                    user.setName(resultSet.getString("name"));
+                    user.setLastName(resultSet.getString("lastName"));
+                    user.setAge(resultSet.getByte("age"));
+                    userList.add(user);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
         return userList;
     }
 
@@ -87,9 +107,14 @@ public class UserDaoJDBCImpl implements UserDao {
     public void cleanUsersTable() {
         try (Connection connection = Util.getConnection();
              Statement statement = connection.createStatement()) {
-            statement.executeUpdate("TRUNCATE users");
+            try {
+                statement.executeUpdate("TRUNCATE users");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                connection.rollback();
+            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 }
